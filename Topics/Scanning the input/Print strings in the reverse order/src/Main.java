2ea1b909e776185d7c49b38bf607d4fdd25d501a
Jeryl Estopace <82858959+pdax-jeryl@users.noreply.github.com>
2021-06-30 09:48:16 +0800

    Scanning Input

--- Topics/Scanning the input/Print strings in the reverse order/src/Main.java
+++ Topics/Scanning the input/Print strings in the reverse order/src/Main.java
@@ -7,16 +7,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         // put your code here
+        ArrayList<String> tokens = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
-
-        while (scanner.hasNext()) {
-            names.add(scanner.next());
+        for (int i = 0; i < 3; i++) {
+            tokens.add(scanner.next());
         }
-
-        Collections.reverse(names);
-        for (String name: names) {
-            System.out.println(name);
+        Collections.reverse(tokens);
+        for (String token: tokens) {
+            System.out.println(token);
         }
     }
 }
